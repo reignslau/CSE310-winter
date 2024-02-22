d4f4ac2c071d61911a162c81583f298879284545
reigns liu <reignsvliu@gmail.com>
2024-02-22 00:58:06 -0700

    2

---
+++ Module3/esay.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Simple Calculator");
+        System.out.println("Enter two numbers:");
+
+        System.out.print("Number 1: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Number 2: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Select operation (+, -, *, /):");
+        char operation = scanner.next().charAt(0);
+
+        double result;
+
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Cannot divide by zero.");
+                }
+                break;
+            default:
+                System.out.println("Invalid operation.");
+        }
+
+        scanner.close();
+    }
+}
